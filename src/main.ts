8d1136d74eec7df2a6989b78f372bacd5b254ece
Rich <miinforma@googlemail.com>
2017-03-07 18:07:05 +1300

    Add debug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Observable } from 'rxjs';
 
 // AppModule
 import { AppModule } from './app';
 
 // common styles
 import './common/styles.scss';
+
+const debuggerOn = true;
+
+Observable.prototype.debug = function (message:string) {
+return this.do(
+  nextValue => {
+    if(debuggerOn)
+    {
+      console.log(message, nextValue);
+    }
+  },
+  error => {
+     if(debuggerOn) {
+       console.error(message, error)
+     }
+  },
+  ()=> {
+      if(debuggerOn) {
+       console.log('boservable compelte d - ', message);
+     }
+  }
+);
+}
+
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    debug: (...any) => Observable<T>
+  }
+}
 
 
 if (process.env.NODE_ENV === 'production') {
